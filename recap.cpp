25dc40c67e7da09e3af1e3297734ce0b09fb073a
Gabriel G <gabigaler50@gmail.com>
2019-01-20 12:05:34 +0200

    added instructions of code

---
+++ recap.cpp
@@ -0,0 +1,41 @@
+/*
+
+	*** MAIN PROGRAM: (cu x inseamna ca am inteles) ***
+	ORDER OF OPERATIONS: 
+
+	1) x - citeste switchuri
+	2) reset camera
+	3) grab camera frame
+	4) derivate line scan
+	5) adjust lights 
+	6) find edges
+	7) review edges
+	8) act on track
+	9) feedback lights
+	10) speed control
+	11) drive
+	12) final: dump data
+
+	_______________________________
+	INSTRUCTIONS:
+
+	- dumpData: printezi toate frameurile care au fost inregistrare de masina.
+	- captureData: incarca fiecare frame cu informatii:
+		* index
+		* line position
+		* steer setting
+		* left/right setting
+		* max nr. of frammes = macro.
+	- readSwitches:
+		* switch 2 ON/OFF: log data/don't log.
+		* switch 3 ON/OFF: risky driving/ don't.
+		* switch 4 ON/OFF: calibrate start/stop gate.
+	- daca switch 2 e ON si go = false (?), atunci tii apasat B si vedem datele.
+		-> vezi cand go devine false in MCP
+	- terminal output: 	* 0 = no terminal output
+						* 1 = frame rate only
+						* 2 = time of operations
+						* 3 = output with delay (?)
+	- pentru a porni/opri: buton A
+
+*/
