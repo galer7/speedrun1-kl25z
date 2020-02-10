80e2f69db98abc92c424c60d4114ea3f3756e8ba
Gabi Galer <gabigaler50@gmail.com>
2020-02-10 23:02:20 +0200

    2020 commit

--- recap.cpp
+++ recap.cpp
@@ -1,41 +1,20 @@
 /*
 
-	*** MAIN PROGRAM: (cu x inseamna ca am inteles) ***
-	ORDER OF OPERATIONS: 
+    CONTROL TEST:
+        - Switch 1 OFF
+        Swtich 1/2/3:
+            -> OFF OFF OFF - LED-uri aprins/deschis
+            -> OFF ON OFF - Control Servo.
+            -> OFF OFF ON - Motoare
+            -> OFF ON ON - Camera
+    
+    Main Program:
+        - Switch 1 ON
+            -> Switch 2 ON/OFF = Log data or not
+            -> Switch 3 ON/OFF = Agresivitate algoritm
+            -> Switch 4 ON/OFF = Control start gate
+            
+    partea dreapta: 6 filete
+    eliminam primii 15-20 pixeli
 
-	1) x - citeste switchuri (218, declaration: 391)
-	2) x - reset camera (233 - 236)
-	3) x  - grab camera frame (239, declaration: 413)
-	4) derivate line scan (248, declaration: 522)
-	5) adjust lights 
-	6) find edges
-	7) review edges
-	8) act on track
-	9) feedback lights
-	10) speed control
-	11) drive
-	12) final: dump data
-
-	_______________________________
-	INSTRUCTIONS:
-
-	- dumpData: printezi toate frameurile care au fost inregistrare de masina.
-	- captureData: incarca fiecare frame cu informatii:
-		* index
-		* line position
-		* steer setting
-		* left/right setting
-		* max nr. of frames = macro.
-		* max nr. of frammes = macro.
-	- readSwitches:
-		* switch 2 ON/OFF: log data/don't log.
-		* switch 3 ON/OFF: risky driving/ don't.
-		* switch 4 ON/OFF: calibrate start/stop gate.
-	- daca switch 2 e ON si go = false (?), atunci tii apasat B si vedem datele.
-		-> vezi cand go devine false in MCP
-	- terminal output: 	* 0 = no terminal output
-						* 1 = frame rate only
-						* 2 = time of operations
-						* 3 = output with delay (?)
-	- pentru a porni/opri: buton A
 */
